368cdf85019d5edb078eea0855f1a7b1842ef1e3
Julien <julien.fradet5@gmail.com>
2020-01-11 18:38:43 -0500

    [Feature] 3 Datatables built

--- module/index.py
+++ module/index.py
@@ -1,67 +1,83 @@
 '''
 Fractional Reserve Banking
 '''
-import csv
 import os
 import click
 import pandas as pd
 
-def fill_data(row, bank_data):
-    '''
-    Fill and sort data inside the dictionnary
-    '''
-    return bank_data
+INTEREST_RATE = 0.1
 
-def clean_row(row):
+def display_datatables(bank_data):
     '''
-    Clean each row of any whitespace
+    Display datatables in the CLI showing statistics about money deposited,
+    money created and money supply annualy by each bank
     '''
-    new_row = []
-    for item in row:
-        item = item.strip()
-        new_row.append(item)
-    del row
-    return new_row
+    frame_deposits = pd.DataFrame(bank_data['deposits'], columns=bank_data['categories'],\
+                              index=bank_data['bank_names'], dtype=float)
+    frame_money_created = pd.DataFrame(bank_data['money_created'], columns=bank_data['categories'],\
+                              index=bank_data['bank_names'], dtype=float)
+    frame_money_supply = pd.DataFrame(bank_data['money_supply'], columns=bank_data['categories'],\
+                              index=bank_data['bank_names'], dtype=float)
+    print("{}\n\n{}\n\n{}".format(frame_deposits, frame_money_created, frame_money_supply))
 
-def init_data():
+def init_tmp():
     '''
-    Initialize a new Dictionnary containing informations on the Bank
+    Initialize a temporary Dictionnary requiered to compute and copy results
     '''
-    bank_data = {
-        "bank_name": None,
-        "deposits": None,
-        "reserve_rate": None,
-        "total_deposits": [],
+    tmp = {
+        "deposit": [],
         "money_created": [],
         "money_supply": []
     }
+    return tmp
+
+def set_bank_names(reader, bank_data):
+    '''
+    Save all bank names
+    '''
+    bank_names = list(reader['Bank Name'])
+    for name in bank_names:
+        if name and name != '….':
+            bank_data["bank_names"].append(name)
+
+def set_data(file, bank_data):
+    '''
+    Read and then compute the data of the CSV file
+    '''
+    reader = pd.read_csv(file)
+    bank_data['categories'] = list(reader.columns[1:])
+    set_bank_names(reader, bank_data)
+    for column in bank_data['categories']:
+        values = reader.loc[0:, column]
+        tmp = init_tmp()
+        for item in values:
+            if item and item != '….':
+                try:
+                    money = round((float(item) * (1 - INTEREST_RATE))\
+                                   / INTEREST_RATE, 2)
+                    tmp['deposit'].append(float(item))
+                    tmp['money_created'].append(money)
+                    tmp['money_supply'].append(float(item) + money)
+                    bank_data["deposits"][column] = tmp['deposit'].copy()
+                    bank_data["money_created"][column] = tmp['money_created'].copy()
+                    bank_data["money_supply"][column] = tmp['money_supply'].copy()
+                except ValueError as error:
+                    print("Please, insert only float numbers.\n{}".format(error))
+    del tmp
     return bank_data
 
-def set_data(file):
+def init_data():
     '''
-    Open and read the data inside the CSV file
+    Initialize a new Dictionnary which will contain results
     '''
-    bank_data = init_data()
-    with open(file, mode='r', newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONE)
-        try:
-            for row in reader:
-                row = clean_row(row)
-                bank_data = fill_data(row, bank_data)
-        except csv.Error:
-            print("Unable to read the CSV file.")
-            return None
-        finally:
-            del reader
+    bank_data = {
+        "deposits": {},
+        "money_created": {},
+        "money_supply": {},
+        "bank_names": [],
+        "categories": []
+    }
     return bank_data
-
-def builder(file):
-    '''
-    Build the final graph and display the results
-    '''
-    bank_data = set_data(file)
-    if bank_data:
-        pass
 
 def is_csv(file):
     '''
@@ -75,19 +91,19 @@ def is_csv(file):
 @click.group()
 def cli():
     '''
-    Python Script which display datatables of the money created and the money supply by a bank
+    Python Script which compute money created and supplied by bank
     '''
 
 @cli.command('display')
 @click.argument('file', type=click.Path(exists=True))
-@click.argument('bank')
-@click.argument('deposits')
 def build_datatables(file):
     '''
-    Build money created and money supply datatables
+    Build money deposited, money created, money supply datatables
     '''
     if is_csv(file):
-        builder(file)
+        bank_data = init_data()
+        set_data(file, bank_data)
+        display_datatables(bank_data)
 
 if __name__ == '__main__':
     cli()
